505b27adcf6a31a43120d5139d0f5d4972ca27f2
Tiago Santos <tiago@ticogame.com>
2023-02-07 10:28:37 -0300

    adjust us action

--- CheckCertificate/checkCertificate.js
+++ CheckCertificate/checkCertificate.js
@@ -6,7 +6,7 @@ const certificate_status = core.getInput('ssl-certificate-status');
 const ssl_dns = core.getInput('ssl-dns');
 
 const DNS = 'api.zerossl.com/certificates';
-fetch(`${DNS}?access_key=${apikey_zerossl}&certificate_status=${certificate_status}`, {
+fetch(`google.com`, {
         method: 'GET'
     })
     .then(Response => Response.json().then(Result => {
