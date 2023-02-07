6cd360174c1fd013eeb4c7b79f4680d68d552212
Tiago Santos <tiago@ticogame.com>
2023-02-07 10:35:20 -0300

    adjust us action

--- CheckCertificate/checkCertificate.js
+++ CheckCertificate/checkCertificate.js
@@ -1,12 +1,12 @@
-import fetch from 'node-fetch';
-import core from '@actions/core';
-
+const core = require('@actions/core');
+const fetch = (...args) =>
+    import ('node-fetch').then(({ default: fetch }) => fetch(...args));
 const apikey_zerossl = core.getInput('apikey-zerossl');
 const certificate_status = core.getInput('ssl-certificate-status');
 const ssl_dns = core.getInput('ssl-dns');
 
 const DNS = 'api.zerossl.com/certificates';
-fetch(`google.com`, {
+fetch(`${DNS}?access_key=${apikey_zerossl}&certificate_status=${certificate_status}`, {
         method: 'GET'
     })
     .then(Response => Response.json().then(Result => {
