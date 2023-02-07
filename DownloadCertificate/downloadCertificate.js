90b16bdccbdcde98a0cf81296e882018ae8a7f94
Tiago Santos <tiago@ticogame.com>
2023-02-07 19:18:07 -0300

    adjust us action

---
+++ DownloadCertificate/downloadCertificate.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+
+const apikey_zerossl = core.getInput('apikey-zerossl');
+const ssl_id = core.get('ssl-id');
+const ssl_path = core.get('ssl-path');
+
+const DNS = 'api.zerossl.com/certificates';
+
+
+
+fetch(`${DNS}/${ssl_id}/download/return?access_key=${apikey_zerossl}`, {
+        method: 'GET'
+    })
+    .then(Response => Response.json().then(Result => {
+        try {
+            var fs = require('fs');
+            fs.appendFile(`${ssl_path}/certificate.crt`, Result['certificate.crt'], function(err) {
+                if (err) throw err;
+                console.log('Saved!');
+            });
+            fs.appendFile(`${ssl_path}/certificate.crt`, Result['ca_bundle.crt'], function(err) {
+                if (err) throw err;
+                console.log('Saved!');
+            });
+        } catch (e) {
+            core.setFailed(Result.error);
+        }
+    }).catch(e => core.setFailed("To transform response into json")))
+    .catch(e => core.setFailed("Failed when trying to request certificate verification"))
