66ec1b9f69d6fa0750a2eecc803cad8b4c0e73bb
Tiago Santos <tiago@ticogame.com>
2023-02-07 10:48:51 -0300

    adjust us action

--- CheckCertificate/checkCertificate.js
+++ CheckCertificate/checkCertificate.js
@@ -16,5 +16,5 @@ fetch(`${DNS}?access_key=${apikey_zerossl}&certificate_status=${certificate_stat
                 core.setOutput('check-json', JSON.stringify(el));
             }
         });
-    }).catch(Resulterror => { throw new Error("Error get response api", Resulterror); }))
+    }).catch(Resulterror => { console.error("Error get response api", Resulterror); }))
     .catch(error => { throw ("Error request get certificates", error); })
