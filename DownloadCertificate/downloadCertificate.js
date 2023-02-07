b17e22124979f6513511f5f9aec39dac4309af10
Tiago Santos <tiago@ticogame.com>
2023-02-07 20:19:09 -0300

    adjust us action

--- DownloadCertificate/downloadCertificate.js
+++ DownloadCertificate/downloadCertificate.js
@@ -17,18 +17,21 @@ fetch(`${DNS}/${ssl_id}/download/return?access_key=${apikey_zerossl}`, {
     .then(Response => Response.json().then(Result => {
         try {
             var fs = require('fs');
-            fs.mkdir(`${ssl_path}`, 0722, (err) => {
-                console.log('create!');
-
-            })
+            if (!fs.existsSync(ssl_path))
+                fs.mkdir(`${ssl_path}`, 0722, (err) => {
+                    core.info(`directory created successfully ( ${ssl_path} )`);
+                })
             fs.writeFile(`${ssl_path}/certificate.crt`, Result['certificate.crt'], function(err) {
                 if (err) throw err;
-                console.log('Saved!');
+                console.log('certificate.crt successfully downloaded!');
+                core.setOutput('certificate', `${ssl_path}/certificate.crt`)
+
             });
-            // fs.writeFile(`${ssl_path}/certificate2.crt`, Result['ca_bundle.crt'], function(err) {
-            //     if (err) throw err;
-            //     console.log('Saved!');
-            // });
+            fs.writeFile(`${ssl_path}/ca_bundle.crt`, Result['ca_bundle.crt'], function(err) {
+                if (err) throw err;
+                console.log('ca_bundle.crt successfully downloaded!');
+                core.setOutput('ca_bundle', `${ssl_path}/ca_bundle.crt`)
+            });
         } catch (e) {
             core.setFailed(e.message);
         }
